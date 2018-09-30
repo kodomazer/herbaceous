c6507441e8e5243023a04848e140425377c8371b
ambersz <ambersz@gmail.com>
2018-09-30 00:17:14 -0700

    pass to play

--- src/components/HerbClient.jsx
+++ src/components/HerbClient.jsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import React , {Component} from 'react';
 import { Client } from 'boardgame.io/react';
 import makeGame from '../game/makeGame';
 import Board from './board';
 
-export default ({playerCount}) => {
-  const ClientWithPlayers = Client({
-    game: makeGame(playerCount),
-    board: Board,
-    // multiplayer: true,
-    // debug: false,
-  });
-  return <ClientWithPlayers playerID={'0'}/>;
+class PassToPlay extends Component {
+  constructor(props) {
+    super(props);
+
+    this.ClientWithPlayers = Client({
+      game: makeGame(this.props.playerCount),
+      board: Board,
+      // multiplayer: true,
+      // debug: false,
+    });
+    this.state = {
+      playerNum: 0,
+    }
+  }
+
+  passPlayer = (e) => {
+    e.preventDefault();
+    var nextPlayer = this.state.playerNum + 1;
+    if (nextPlayer >= this.props.playerCount) {
+      nextPlayer = 0;
+    }
+    this.setState({playerNum:nextPlayer});
+  }
+
+  render() {
+    var ClientWithPlayers = this.ClientWithPlayers;
+    return (
+      <div>
+        <button onClick={this.passPlayer}> Pass Board To Next Player</button>
+        <ClientWithPlayers playerID={this.state.playerNum.toString()}/>
+      </div>
+    );
+  }
 }
+
+export default PassToPlay
